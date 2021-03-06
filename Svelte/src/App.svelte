abc62e4cf46da61bdf24e10be90abe41a8eb1d6e
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-06 07:08:49 +0530

    Added search bar component

--- Svelte/src/App.svelte
+++ Svelte/src/App.svelte
@@ -1,11 +1,16 @@
 <script>
 	import Header from './components/Header.svelte';
+	import SearchBar from './components/SearchBar.svelte';
 </script>
 
 <div class="container">
 	<Header />
+	<SearchBar />
 </div>
 
 <style>
-	
+	.container {
+		width: 80%;
+		margin: 0 auto;
+	}
 </style>
---
+++ Svelte/src/components/SearchBar.svelte
@@ -0,0 +1,75 @@
+<div class="search">
+    <form id="searchForm">
+        <div class="search-bar">
+            <input type="text" id="searchInput" class="search-input" autocomplete="off">
+            <button type="submit" class="btn-search">Woof</button>
+        </div>
+    </form>
+
+    <div id="searchResults" class="search-results"></div>
+</div>
+
+<style>
+    .search {
+        width: 100%;
+        margin-bottom: 20px;
+    }
+
+    .search-bar {
+        display: flex;
+        justify-content: center;
+    }
+
+    .search-input {
+        width: 80%;
+        font-size: 1rem;
+        padding: 10px;
+        border-radius: 10px;
+        outline: none;
+        margin: 0 10px;
+    }
+
+    .btn-search {
+        width: 10%;
+        font-size: 1.5rem;
+        border-radius: 10px;
+        border: 2px solid black;
+        outline: none;
+        background: rgba(48, 233, 48, 0.664);
+        color: white;
+        cursor: pointer;
+        overflow: hidden;
+    }
+
+    .search-results {
+        width: 60%;
+        display: flex;
+        flex-direction: column;
+        margin-top: 10px;
+        margin-left: 10%;
+        max-height: 20rem;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        padding-right: 10px;
+    }
+
+    .search-results::-webkit-scrollbar {
+        background: rgba(31, 30, 30, 0.80);
+        border-radius: 10px;
+    }
+
+    .search-results::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        background: #333;
+    }
+
+    .search-result {
+        width: 100%;
+        padding: 10px;
+        font-size: 1.5rem;
+        background: rgba(31, 30, 30, 0.80);
+        margin: 2px;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+</style>
