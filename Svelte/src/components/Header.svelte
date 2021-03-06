312469f2216fe6fcb83fe3a029c2f8d95fe46f5b
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-06 07:04:01 +0530

    Added header component

---
+++ Svelte/src/components/Header.svelte
@@ -0,0 +1,12 @@
+<header class="heading">
+    <h1>Dogzzz</h1>
+</header>
+
+<style>
+    .heading {
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        font-size: 2rem;
+    }
+</style>
